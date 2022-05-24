6afb44a15ec2085bcde04182e54ec9e10ef3aab9
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-25 03:47:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lr5az.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+        await client.connect();
+        const toolsCollection = client.db('final_project').collection('tools')
+
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+    }
+    finally {
+
+    }
+
+
+}
+
+
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from final project')
+})
+
+app.listen(port, () => {
+    console.log(`Final project app listening on port ${port}`)
+})
