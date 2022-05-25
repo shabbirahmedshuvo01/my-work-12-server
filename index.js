bf2b5efe8394dab36c7d9effc9062caf7ba41393
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-25 19:23:56 +0600

    second  api set

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -30,7 +30,14 @@ async function run() {
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
-        })
+        });
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const book = await toolsCollection.findOne(query);
+            res.send(book);
+        });
 
     }
     finally {
