76324e655006da9d5850ba39a7b1345b5e834c4e
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-26 23:39:22 +0600

    added payment api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 app.use(cors());
@@ -55,6 +56,19 @@ async function run() {
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
+        });
+
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const tools = req.body;
+            const price = tools.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
         });
 
 
