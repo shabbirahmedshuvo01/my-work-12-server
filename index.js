89c45e9090a52b3ff13546f18a7fe216dc1ba26b
unknown <sabbirshuvo006@gmail.com>
2022-12-06 02:05:30 +0600

    new link created

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lr5az.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://final_admin:P3UxAGvoIKoGk78a@cluster0.lr5az.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
